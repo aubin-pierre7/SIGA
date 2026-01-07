8151318993109ac36b399785cd692e0d85e9ea3d
aubin-pierre7 <aubinp58@gmail.com>
2026-01-07 23:12:54 +0100

    Ajout de MainLayout dans AppRoutes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,13 +1,22 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "../pages/Login/Login";
 import Dashboard from "../pages/Dashboard/Dashboard";
+import MainLayout from "../layouts/MainLayout";
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+
+      <Route
+        path="/dashboard"
+        element={
+          <MainLayout>
+            <Dashboard />
+          </MainLayout>
+        }
+      />
     </Routes>
   );
 }
