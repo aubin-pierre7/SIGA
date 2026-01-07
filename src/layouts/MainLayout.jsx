eddcec1efc22f3e5fdfaa16853b67c9c6de173e3
aubin-pierre7 <aubinp58@gmail.com>
2026-01-07 23:11:39 +0100

    Modification de MainLayout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,18 @@
+import { Link } from "react-router-dom";
+
 function MainLayout({ children }) {
   return (
     <div>
-      <header>
+      <header style={{ padding: "10px", background: "#eee" }}>
         <h2>Système d’Archive Numérique</h2>
+
+        <nav>
+          <Link to="/dashboard">Dashboard</Link> |{" "}
+          <Link to="/documents">Documents</Link>
+        </nav>
       </header>
 
-      <main>
+      <main style={{ padding: "20px" }}>
         {children}
       </main>
     </div>
