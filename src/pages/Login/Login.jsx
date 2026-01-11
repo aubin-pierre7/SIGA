936fbe781f749c504732332fff6d955bb7fc8783
aubin-pierre7 <aubinp58@gmail.com>
2026-01-11 17:57:36 +0100

    Modification de la page de connexio

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,26 @@
+import "./Login.css";
+
 function Login() {
   return (
-    <div>
-      <h1>Page de connexion</h1>
-      <p>Bienvenue sur le système d’archive numérique</p>
+    <div className="login-container">
+      <div className="login-box">
+        <h1>Connexion</h1>
+        <p>Système d’Archive Numérique</p>
+
+        <form>
+          <div className="form-group">
+            <label>Email</label>
+            <input type="email" placeholder="exemple@email.com" />
+          </div>
+
+          <div className="form-group">
+            <label>Mot de passe</label>
+            <input type="password" placeholder="********" />
+          </div>
+
+          <button type="submit">Se connecter</button>
+        </form>
+      </div>
     </div>
   );
 }
